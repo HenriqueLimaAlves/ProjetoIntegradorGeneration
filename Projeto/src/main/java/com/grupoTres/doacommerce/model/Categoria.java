f0f9eda27b63d69d1c558974267467e1a4053dd6
Hnerique <henri633@gmail.com>
2020-09-03 17:44:57 -0300

    Projeto e Documentacao - Task 3 e 4 - 03-09-2020

--- Projeto/src/main/java/com/grupoTres/doacommerce/model/Categoria.java
+++ Projeto/src/main/java/com/grupoTres/doacommerce/model/Categoria.java
@@ -19,31 +19,7 @@ public class Categoria {
 	
 	@NotNull
 	@Size(max = 250)
-	private String alimentos;
-	
-	@NotNull
-	@Size(max = 250)
-	private String roupas;
-	
-	@NotNull
-	@Size(max = 250)
-	private String moveis;
-	
-	@NotNull
-	@Size(max = 250)
-	private String utensilios;
-	
-	@NotNull
-	@Size(max = 250)
-	private String higiene;
-	
-	@NotNull
-	@Size(max = 250)
-	private String educacao_cultura;
-	
-	@NotNull
-	@Size(max = 250)
-	private String urgente;
+	private String descricao;
 
 	public long getId() {
 		return id;
@@ -53,59 +29,11 @@ public class Categoria {
 		this.id = id;
 	}
 
-	public String getAlimentos() {
-		return alimentos;
+	public String getDescricao() {
+		return descricao;
 	}
 
-	public void setAlimentos(String alimentos) {
-		this.alimentos = alimentos;
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
-
-	public String getRoupas() {
-		return roupas;
-	}
-
-	public void setRoupas(String roupas) {
-		this.roupas = roupas;
-	}
-
-	public String getMoveis() {
-		return moveis;
-	}
-
-	public void setMoveis(String moveis) {
-		this.moveis = moveis;
-	}
-
-	public String getUtensilios() {
-		return utensilios;
-	}
-
-	public void setUtensilios(String utensilios) {
-		this.utensilios = utensilios;
-	}
-
-	public String getHigiene() {
-		return higiene;
-	}
-
-	public void setHigiene(String higiene) {
-		this.higiene = higiene;
-	}
-
-	public String getEducacao_cultura() {
-		return educacao_cultura;
-	}
-
-	public void setEducacao_cultura(String educacao_cultura) {
-		this.educacao_cultura = educacao_cultura;
-	}
-
-	public String getUrgente() {
-		return urgente;
-	}
-
-	public void setUrgente(String urgente) {
-		this.urgente = urgente;
-	}	
 }
